d514b3a646516d0d27dbb4cc43ae329a2233a7f6
Nemanja1208 <nevaljao994@gmail.com>
2023-08-15 14:21:53 +0200

    init

---
+++ InlUpp1/Models/Advertisment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InlUpp1.Models
+{
+    internal class Advertisment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+    }
+}
---
+++ InlUpp1/Program.cs
@@ -0,0 +1,93 @@
+﻿// SAVING ANNONS APP
+// ANNONS STRUCTURE - TITLE, DESCRIPTION, PRICE, CATEGORY
+// YOU SHOULD BE ABLE TO CHOOSE CATEGORY
+// CATEGORIES - Furniture, Mobil, Bicycle, Computers etc
+// Create Read Update Delete Actions with DATABASE
+// Search functionality based on Title and Category - should return list
+
+
+using InlUpp1.Models;
+
+bool applicationRunning = true;
+
+while(applicationRunning)
+{
+    Console.WriteLine("Welcome to Blocket 2.0");
+    Console.WriteLine("Available Options:");
+    Console.WriteLine("(1) - Add new Advertisment");
+    Console.WriteLine("(2) - Edit existing Advertisment");
+    Console.WriteLine("(3) - Delete Advertisment");
+    Console.WriteLine("(4) - Search Advertisment according to Title");
+    Console.WriteLine("(5) - Search Advertisment according to Category");
+    Console.WriteLine("Enter the command number: ");
+
+    ConsoleKey userEntryValue = Console.ReadKey().Key;
+
+    switch (userEntryValue)
+    {
+        case ConsoleKey.D1:
+            // PROCESS OF CREATING A ADVERTISMENT
+            // advertisment is a object that has its properties so we create a class
+
+            bool createAdvertismentInProgress = true;
+
+            List<Advertisment> advertisements = new List<Advertisment>();
+
+            Advertisment newAdvertisment = new Advertisment();
+
+            bool titleIsSet = false;
+            bool descriptionIsSet = false;
+            bool priceIsSet = false;
+
+
+            if (!string.IsNullOrEmpty(newAdvertisment.Title))
+            {
+                Console.WriteLine($"\n Advertisement Title: {newAdvertisment.Title}");
+            }
+
+            while (createAdvertismentInProgress)
+            {
+                if (!string.IsNullOrEmpty(newAdvertisment.Title))
+                {
+                    Console.WriteLine($"\n Advertisement Title: {newAdvertisment.Title}");
+                } else
+                {
+                    Console.WriteLine(" \n What is the title for your advertisment");
+                    newAdvertisment.Title = Console.ReadLine();
+                    titleIsSet = true;
+                }
+
+                if (!string.IsNullOrEmpty(newAdvertisment.Description) && titleIsSet)
+                {
+                    Console.WriteLine($"\nAdvertisement Description: {newAdvertisment.Description}");
+                }
+                else
+                {
+                    Console.WriteLine(" \n What is the description for your advertisment");
+                    newAdvertisment.Description = Console.ReadLine();
+                    descriptionIsSet = true;
+                }
+
+                if (!string.IsNullOrEmpty(newAdvertisment.Price) && priceIsSet && descriptionIsSet)
+                {
+                    Console.WriteLine($"\n Advertisement Price: {newAdvertisment.Price}");
+                }
+                else
+                {
+                    Console.WriteLine(" \n What is the price for your advertisment");
+                    newAdvertisment.Price = Console.ReadLine();
+                    priceIsSet = true;
+                }
+
+
+                //createAdvertismentInProgress = false;
+            }
+            
+            break;
+        default:
+            Console.WriteLine(" \n Not a valid option");
+            break;
+    }
+}
+
+
